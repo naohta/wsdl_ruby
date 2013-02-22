f917207d612a5dff9e9fa22e5a3762e599a33014
naohta <naohta@gmail.com>
2013-02-22 14:13:24 +0900

    mmm

--- web.rb
+++ web.rb
@@ -10,7 +10,7 @@ end
 Viewpoint::EWS::EWS.endpoint = 'https://mail.agricom.co.jp/EWS/Services.wsdl'
 user = ENV['ews_user']
 pass = ENV['ews_pass']
-p user
-p pass
 
 Viewpoint::EWS::EWS.set_auth user,pass
+inbox = Viewpoint::EWS::Folder.get_folder_by_name('Inbox')
+p inbox
